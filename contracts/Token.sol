fdf3a0d281567f8090d9314293cc05d9f1dbf71c
Bhautik Bhalala <47077595+Bhautik-Bhalala@users.noreply.github.com>
2022-11-02 17:19:08 +0530

    initial -2

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token{
+    string name = "Liquidity";
+
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,6 +3,20 @@ require("dotenv").config();
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    localhost: {}
+  },
 };
