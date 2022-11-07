287d48346d50a32667a16ecc8ff05c02d7ee9875
Bhautik Bhalala <47077595+Bhautik-Bhalala@users.noreply.github.com>
2022-11-07 13:56:01 +0530

    Cancel rders and its tests

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange{
     mapping(address => mapping(address => uint256)) public tokens;
     mapping (uint256 => _Order) public orders;
     uint256 public ordersCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     //A way to model the order
     struct _Order{
@@ -29,6 +30,15 @@ contract Exchange{
     event Withdraw(address token , address user , uint256 amount , uint256 balance);
 
     event Order(        
+        uint256 id, 
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp);
+
+        event Cancel(        
         uint256 id, 
         address user,
         address tokenGet,
@@ -117,5 +127,27 @@ contract Exchange{
              _tokenGive, 
              _amountGive, 
              block.timestamp);
+    }
+
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+
+        orderCancelled[_id] = true;
+
+        require(address(_order.user) == msg.sender);
+
+        require(_order.id == _id);
+
+        //emit event 
+             emit Cancel(
+                _order.id, 
+             msg.sender, 
+             _order.tokenGet, 
+             _order.amountGet, 
+             _order.tokenGive, 
+             _order.amountGive, 
+             block.timestamp);
+
+
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -22,6 +22,8 @@ describe('Exchange' , () => {
         deployer = accounts[0]
         feeAccount = accounts[1]
         user1 = accounts[2]
+        user2 = accounts[3]
+
 
         let transaction = await token1.connect(deployer).transfer(user1.address,tokens(100))
 
@@ -198,5 +200,79 @@ describe('Exchange' , () => {
         })
 
     })
+
+    describe("Order actions" , async() =>{
+
+        let transaction , result
+        let amount = tokens(1)
+
+        beforeEach(async () => {
+            transaction = await token1.connect(user1).approve(exchange.address,amount) 
+            result = await transaction.wait();
+
+            transaction = await exchange.connect(user1).depositToken(token1.address,amount) 
+            result = await transaction.wait();
+
+            transaction = await exchange.connect(user1).makeOrder(
+                token2.address , amount, token1.address , amount
+                );
+            result = await transaction.wait();
+        })
+        describe("Cancelling orders" , async () =>{
+            describe("Success" , async () =>{
+                beforeEach(async () =>{
+                    transaction = await exchange.connect(user1).cancelOrder(1)
+                    result = await transaction.wait()
+                })
+
+                it("updates cancelled orders" , async () =>{
+                    expect(await exchange.orderCancelled(1)).to.equal(true)
+                })
+                it("Emits a Order Cancel EVENT" , async() => 
+                {
+                    const event = result.events[0]
+                    expect(event.event).to.equal('Cancel')
+        
+                    const args = event.args
+                    expect(args.id).to.equal(1)
+                    expect(args.user).to.equal(user1.address)
+                    expect(args.tokenGet).to.equal(token2.address)
+                    expect(args.amountGet).to.equal(tokens(1))
+                    expect(args.tokenGive).to.equal(token1.address)
+                    expect(args.amountGive).to.equal(tokens(1))
+                    expect(args.timestamp).to.at.least(1)
     
+                })
+
+
+            })
+
+
+            describe("Failure" , async () =>{
+                beforeEach(async () =>{
+                    transaction = await token1.connect(user1).approve(exchange.address,amount) 
+                    result = await transaction.wait();
+        
+                    transaction = await exchange.connect(user1).depositToken(token1.address,amount) 
+                    result = await transaction.wait();
+        
+                    transaction = await exchange.connect(user1).makeOrder(
+                        token2.address , amount, token1.address , amount
+                    );
+                    result = await transaction.wait();
+
+                })
+                it("Rejects invalid order ids" , async () =>{
+                    const invalidOrderId = 9999;
+                    await expect(exchange.connect(user1).cancelOrder(invalidOrderId)).to.be.reverted
+                })
+
+                it("Rejects unauthorised cancellations" , async () =>{
+                    await expect(exchange.connect(user2).cancelOrder(1)).to.be.reverted
+                })
+
+
+            })
+        })
+    })
 })
