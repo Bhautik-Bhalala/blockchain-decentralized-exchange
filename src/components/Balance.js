a4a2dbaae06438394cff29626cdf3c466d0a5ab1
Bhautik Bhalala <47077595+Bhautik-Bhalala@users.noreply.github.com>
2022-11-10 14:09:28 +0530

    DEPOSIT AND WITHDRAW START

---
+++ src/components/Balance.js
@@ -0,0 +1,54 @@
+const Balance = () => {
+
+    return (
+      <div className='component exchange__transfers'>
+        <div className='component__header flex-between'>
+          <h2>Balance</h2>
+          <div className='tabs'>
+            <button className='tab tab--active'>Deposit</button>
+            <button className='tab'>Withdraw</button>
+          </div>
+        </div>
+  
+        {/* Deposit/Withdraw Component 1 (LIQ) */}
+  
+        <div className='exchange__transfers--form'>
+          <div className='flex-between'>
+  
+          </div>
+  
+          <form>
+            <label htmlFor="token0"></label>
+            <input type="text" id='token0' placeholder='0.0000' />
+  
+            <button className='button' type='submit'>
+              <span></span>
+            </button>
+          </form>
+        </div>
+  
+        <hr />
+  
+        {/* Deposit/Withdraw Component 2 (mETH) */}
+  
+        <div className='exchange__transfers--form'>
+          <div className='flex-between'>
+  
+          </div>
+  
+          <form>
+            <label htmlFor="token1"></label>
+            <input type="text" id='token1' placeholder='0.0000'/>
+  
+            <button className='button' type='submit'>
+              <span></span>
+            </button>
+          </form>
+        </div>
+  
+        <hr />
+      </div>
+    );
+  }
+  
+  export default Balance;
